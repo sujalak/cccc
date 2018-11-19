f4f2558af54d62cab3143fd638afdaa4467d1aa3
Sujala Vijay K <sujalavk@gmail.com>
2018-11-19 14:02:10 +0530

    first demo

---
+++ Client.java
@@ -0,0 +1,11 @@
+
+class Client
+{  public static void main(String args[])
+  { 
+    Student student1=new Student();
+    student1.insertStudentDetails();
+    student1.printstudentDetails();
+
+}
+
+}
